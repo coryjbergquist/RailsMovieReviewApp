e73088a6263744be642fa3f54d5f1758a2bafd6f
coryjbergquist <cory.j.bergquist@gmail.com>
2018-08-21 16:12:26 -0700

    added uniqueness validator for user name

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
   has_many :reviews
   has_many :movies, through: :reviews
   validates :name, presence: true
+  validates :name, uniqueness: true
   has_secure_password
 
 
