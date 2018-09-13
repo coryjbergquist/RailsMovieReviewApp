9e820be6f57e3434360c6b79b4897a6ca56cf6ed
coryjbergquist <cory.j.bergquist@gmail.com>
2018-09-13 14:54:51 -0700

    add serialize method to CommentSerializer and use it in user controller

--- app/serializers/review_serializer.rb
+++ app/serializers/review_serializer.rb
@@ -1,3 +1,17 @@
 class ReviewSerializer < ActiveModel::Serializer
   attributes :id, :title, :content, :movie_id
+
+  def self.serialize(reviews)
+    serialized_review = {}
+    reviews.map do |review|
+      serialized_product = '{'
+
+      serialized_product += '"id": ' + review.id.to_s + ', '
+      serialized_product += '"title": "' + review.title + '", '
+      serialized_product += '"content": "' + review.content + '", '
+
+      serialized_product += '}'
+    end
+end
+
 end
