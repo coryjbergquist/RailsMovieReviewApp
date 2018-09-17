493950167e3103939ffb1c287d4a1b30b0f74ce5
coryjbergquist <cory.j.bergquist@gmail.com>
2018-09-16 17:03:08 -0700

    add js reviews tag to movies/new view page

--- app/assets/javascripts/reviews.js
+++ app/assets/javascripts/reviews.js
@@ -3,3 +3,9 @@ function Review(title, content, rating) {
     this.content = content;
     this.rating = rating;
 }
+
+Review.prototype.renderReview = function() {
+  $("#reviewTitle").text(post["title"]);
+  $("#reviewContent").text(post["content"]);
+  $("#reviewRating").text(post["rating"]);
+}
