ac07503742c4896184924ae039e5b551b087fc7c
coryjbergquist <cory.j.bergquist@gmail.com>
2018-09-18 14:46:35 -0700

    add movieTitle to JS Review building and prototype

--- app/assets/javascripts/reviews.js
+++ app/assets/javascripts/reviews.js
@@ -1,10 +1,12 @@
 function Review(data) {
+    this.movieTitle = data["movie"]["title"];
     this.title = data["title"];
     this.content = data["content"];
     this.rating = data["rating"];
 }
 
 Review.prototype.renderReview = function() {
+  $("#movieTitle").text("Review for: " + this["movieTitle"]);
   $("#reviewTitle").text("Title: " + this["title"]);
   $("#reviewContent").text("Content: " + this["content"]);
   $("#reviewRating").text("Rating: " + this["rating"]);
